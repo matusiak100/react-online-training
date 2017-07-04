2190c29496fa1f8234ce02ceda2d73a15cc89151
burczu <b.dybowski@gmail.com>
2017-07-04 10:23:12 +0200

    initialization

--- src/Events.js
+++ src/Events.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Events = (props) => {
-  return (
-    <ul>
-      {props.events.map(item => {
-        const date = new Date(item.date);
+class Events extends React.Component {
+  static propTypes = {
+    events: PropTypes.array.isRequired
+  };
 
-        if (date >= Date.now()) {
-          return (
-            <li key={item.id}>
-              <strong>{item.name}</strong><br />
-              Gdzie: {item.place}<br />
-              Kiedy: {item.date} - {item.time}
-            </li>
-          );
-        }
+  render() {
+    return (
+      <ul>
+        {this.props.events.map(item => {
+          const date = new Date(item.date);
 
-        return null;
-      })}
-    </ul>
-  );
+          if (date >= Date.now()) {
+            return (
+              <li key={item.id}>
+                <strong>{item.name}</strong><br />
+                Gdzie: {item.place}<br />
+                Kiedy: {item.date} - {item.time}
+              </li>
+            );
+          }
+
+          return null;
+        })}
+      </ul>
+    );
+  }
 };
-
-Events.propTypes = {
-  events: PropTypes.array.isRequired
-}
 
 export default Events;
