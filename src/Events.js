635d1d7050a2cb29cba6e29ab3cd25c811cf05ac
burczu <b.dybowski@gmail.com>
2017-07-04 10:32:22 +0200

    initialization

--- src/Events.js
+++ src/Events.js
@@ -13,25 +13,34 @@ class Events extends React.Component {
     });
   }
 
+  onClearClicked(event) {
+    event.preventDefault();
+
+    this.setState({ events: [] });
+  }
+
   render() {
     return (
-      <ul>
-        {this.state.events.map(item => {
-          const date = new Date(item.date);
+      <div>
+        <ul>
+          {this.state.events.map(item => {
+            const date = new Date(item.date);
 
-          if (date >= Date.now()) {
-            return (
-              <li key={item.id}>
-                <strong>{item.name}</strong><br />
-                Gdzie: {item.place}<br />
-                Kiedy: {item.date} - {item.time}
-              </li>
-            );
-          }
+            if (date >= Date.now()) {
+              return (
+                <li key={item.id}>
+                  <strong>{item.name}</strong><br />
+                  Gdzie: {item.place}<br />
+                  Kiedy: {item.date} - {item.time}
+                </li>
+              );
+            }
 
-          return null;
-        })}
-      </ul>
+            return null;
+          })}
+        </ul>
+        <button onClick={this.onClearClicked.bind(this)}>Wyczyść</button>
+      </div>
     );
   }
 };
