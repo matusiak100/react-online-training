9022ba2d25a98878251e60e78049362dd22e3a60
burczu <b.dybowski@gmail.com>
2017-07-04 09:24:38 +0200

    initializations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import events from './data/events.json';
+
 ReactDOM.render(<p>Your App goes here....</p>, document.getElementById('root'));
