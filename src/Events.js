317c170c168243114bb173f568bef79acdd436e2
burczu <b.dybowski@gmail.com>
2017-07-12 15:04:55 +0200

    prop types removed

--- src/Events.js
+++ src/Events.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Events = (props) => {
   return (
@@ -22,9 +21,5 @@ const Events = (props) => {
     </ul>
   );
 };
-
-Events.propTypes = {
-  events: PropTypes.array.isRequired
-}
 
 export default Events;
