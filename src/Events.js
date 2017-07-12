179a68d18511e7df94e9042106fb5f8414d04c12
burczu <b.dybowski@gmail.com>
2017-07-12 15:08:13 +0200

    initialization

--- src/Events.js
+++ src/Events.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class Events extends React.Component {
-  static propTypes = {
-    events: PropTypes.array.isRequired
-  };
-
   render() {
     return (
       <ul>
