ce9668a8c1905c527b5ce6152a86b12f52602397
burczu <b.dybowski@gmail.com>
2017-07-20 11:18:09 +0200

    input value handling added

--- src/EventFilters.js
+++ src/EventFilters.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const EventFilters = (props) => {
-  return <input type="text" onChange={props.onFilterChange} placeholder="filtruj..." />
+  return <input type="text" value={props.filter} onChange={props.onFilterChange} placeholder="filtruj..." />
 };
 
 EventFilters.propTypes = {
+  filter: PropTypes.string.isRequired,
   onFilterChange: PropTypes.func.isRequired
 };
 
